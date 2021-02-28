83e4bac5749f57ebb649b76bda2634eab10c6d83
oliverlsanz <oliverlsanz@gmail.com>
2021-02-28 20:11:00 +0100

    add firebase credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,7 @@
 import * as firebase from 'firebase';
 import '@firebase/auth';
 import '@firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAOWHBpPhKoNhcGFKHH_Q_0AtL2gV-imgQ',
-  authDomain: 'production-a9404.firebaseapp.com',
-  databaseURL: 'https://production-a9404.firebaseio.com',
-  projectId: 'production-a9404',
-  storageBucket: 'production-a9404.appspot.com',
-  messagingSenderId: '525472070731',
-  appId: '1:525472070731:web:ee873bd62c0deb7eba61ce',
-};
+import { firebaseConfig } from './secrets'
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);

e2b09555150351048b96a4aed23755278f3af5e5
oliverlsanz <oliverlsanz@gmail.com>
2021-03-01 16:38:58 +0100

    migrate LoginScren to paper

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
 import {decode, encode} from 'base-64'
+import { Provider as PaperProvider } from 'react-native-paper';
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
@@ -46,19 +47,21 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        { user ? (
-          <Stack.Screen name="Home">
-            {props => <HomeScreen {...props} extraData={user} />}
-          </Stack.Screen>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>  {/* Should be kept inside other providers, since this generates components that may need what they provide */}
+      <NavigationContainer>
+        <Stack.Navigator>
+          { user ? (
+            <Stack.Screen name="Home">
+              {props => <HomeScreen {...props} extraData={user} />}
+            </Stack.Screen>
+          ) : (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Registration" component={RegistrationScreen} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react'
-import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Button, TextInput, Subheading} from 'react-native-paper';
 import styles from './styles';
 import { firebase } from '../../firebase/config'
+
+const SeparatorContainer = ({children, separation}) => {
+    var output = []
+    
+    const shouldComponentUpdate = (nextProps, nextState) => {
+        return false
+      }
+
+    children.forEach((child) => {
+        output.push(child)
+        output.push(<View style={{height: separation}} />)
+    })
+
+    output.pop()
+
+    return output
+}
 
 export default function LoginScreen({navigation}) {
     const [email, setEmail] = useState('')
@@ -41,39 +59,29 @@ export default function LoginScreen({navigation}) {
     return (
         <View style={styles.container}>
             <KeyboardAwareScrollView
-                style={{ flex: 1, width: '100%' }}
+                style={{ flex: 1, width: '100%', padding: 16}}
                 keyboardShouldPersistTaps="always">
                 <Image
                     style={styles.logo}
                     source={require('../../../assets/icon.png')}
                 />
-                <TextInput
-                    style={styles.input}
-                    placeholder='E-mail'
-                    placeholderTextColor="#aaaaaa"
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor="#aaaaaa"
-                    secureTextEntry
-                    placeholder='Password'
-                    onChangeText={(text) => setPassword(text)}
-                    value={password}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
-                />
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => onLoginPress()}>
-                    <Text style={styles.buttonTitle}>Log in</Text>
-                </TouchableOpacity>
-                <View style={styles.footerView}>
-                    <Text style={styles.footerText}>Don't have an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Sign up</Text></Text>
-                </View>
+                <SeparatorContainer separation={12}>
+                    <TextInput
+                        placeholder='E-mail'
+                        onChangeText={(text) => setEmail(text)}
+                        value={email}
+                    />
+                    <TextInput
+                        secureTextEntry
+                        placeholder='Password'
+                        onChangeText={(text) => setPassword(text)}
+                        value={password}
+                    />
+                    <Button onPress={onLoginPress} mode='contained'>
+                    Log In
+                    </Button>
+                    <View style={styles.footer}><Subheading>Don't have an account? <Subheading onPress={onFooterLinkPress} style={styles.footerLink}>Sign up</Subheading></Subheading></View>
+                </SeparatorContainer>
             </KeyboardAwareScrollView>
         </View>
     )
